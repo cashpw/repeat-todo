f75d8d036ddb6cdeab88b95f908e2e9502334e07
Cash Prokop-Weaver <cash@cashpw.com>
2024-12-16 14:21:17 -0800

    🐛 Fix numbers and names; sunday=7

--- repeat-todo-test.el
+++ repeat-todo-test.el
@@ -2,7 +2,7 @@
 (require 'ert)
 
 (defvar repeat-todo--test-weekdays '(1 2 3 4 5))
-(defvar repeat-todo--test-weekends '(0 6))
+(defvar repeat-todo--test-weekends '(6 7))
 
 (defvar repeat-todo--test-monday (date-to-time "2000-01-03T08:00:00-0700"))
 (defvar repeat-todo--test-tuesday (date-to-time "2000-01-04T08:00:00-0700"))
@@ -12,11 +12,15 @@
 (defvar repeat-todo--test-saturday (date-to-time "2000-01-08T08:00:00-0700"))
 (defvar repeat-todo--test-sunday (date-to-time "2000-01-09T08:00:00-0700"))
 (defvar repeat-todo--test-next-monday (date-to-time "2000-01-10T08:00:00-0700"))
-(defvar repeat-todo--test-next-tuesday (date-to-time "2000-01-11T08:00:00-0700"))
-(defvar repeat-todo--test-next-wednesday (date-to-time "2000-01-12T08:00:00-0700"))
-(defvar repeat-todo--test-next-thursday (date-to-time "2000-01-13T08:00:00-0700"))
+(defvar repeat-todo--test-next-tuesday
+  (date-to-time "2000-01-11T08:00:00-0700"))
+(defvar repeat-todo--test-next-wednesday
+  (date-to-time "2000-01-12T08:00:00-0700"))
+(defvar repeat-todo--test-next-thursday
+  (date-to-time "2000-01-13T08:00:00-0700"))
 (defvar repeat-todo--test-next-friday (date-to-time "2000-01-14T08:00:00-0700"))
-(defvar repeat-todo--test-next-saturday (date-to-time "2000-01-15T08:00:00-0700"))
+(defvar repeat-todo--test-next-saturday
+  (date-to-time "2000-01-15T08:00:00-0700"))
 (defvar repeat-todo--test-next-sunday (date-to-time "2000-01-16T08:00:00-0700"))
 
 (defvar repeat-todo--current-time-override-time nil)
@@ -26,83 +30,173 @@
 
 (defmacro with-current-time (new-current-time &rest body)
   "Run body with `current-time' returning NEW-CURRENT-TIME."
-  `(letf (((symbol-function 'current-time)
-           (lambda ()
-             ,new-current-time)))
-     ,@body))
+  `(letf
+       (((symbol-function 'current-time) (lambda () ,new-current-time))) ,@body))
 
-(ert-deftest repeat-todo--next-scheduled-time ()
-  (should
-   (string=
-    (format-time-string
-     "%FT%T%z"
-     (with-current-time repeat-todo--test-monday
-                        (repeat-todo--next-scheduled-time
-                         repeat-todo--test-monday repeat-todo--test-weekdays)))
-    (format-time-string "%FT%T%z" repeat-todo--test-tuesday)))
-  (should
-   (string=
-    (format-time-string
-     "%FT%T%z"
-     (with-current-time repeat-todo--test-tuesday
-                        (repeat-todo--next-scheduled-time
-                         repeat-todo--test-tuesday repeat-todo--test-weekdays)))
-    (format-time-string "%FT%T%z" repeat-todo--test-wednesday)))
-  (should
-   (string=
-    (format-time-string
-     "%FT%T%z"
-     (with-current-time repeat-todo--test-wednesday
-                        (repeat-todo--next-scheduled-time
-                         repeat-todo--test-wednesday repeat-todo--test-weekdays)))
-    (format-time-string "%FT%T%z" repeat-todo--test-thursday)))
-  (should
-   (string=
-    (format-time-string
-     "%FT%T%z"
-     (with-current-time repeat-todo--test-thursday
-                        (repeat-todo--next-scheduled-time
-                         repeat-todo--test-thursday repeat-todo--test-weekdays)))
-    (format-time-string "%FT%T%z" repeat-todo--test-friday)))
-  (should
-   (string=
-    (format-time-string
-     "%FT%T%z"
-     (with-current-time repeat-todo--test-friday
-                        (repeat-todo--next-scheduled-time
-                         repeat-todo--test-friday repeat-todo--test-weekdays)))
-    (format-time-string "%FT%T%z" repeat-todo--test-next-monday)))
+(defmacro time-string= (time-a time-b)
+  "Compare human-readable TIME-A and TIME-B."
+  `(string=
+    (format-time-string "%FT%T%z" ,time-a)
+    (format-time-string "%FT%T%z" ,time-b)))
 
+(ert-deftest repeat-todo--next-scheduled-time--weekdays ()
+  ;; Weekdays
   (should
-   (string=
-    (format-time-string
-     "%FT%T%z"
-     (with-current-time repeat-todo--test-saturday
-                        (repeat-todo--next-scheduled-time
-                         repeat-todo--test-saturday repeat-todo--test-weekends)))
-    (format-time-string "%FT%T%z" repeat-todo--test-sunday)))
+   (time-string=
+    (with-current-time repeat-todo--test-monday
+                       (repeat-todo--next-scheduled-time
+                        repeat-todo--test-monday repeat-todo--test-weekdays))
+    repeat-todo--test-tuesday))
   (should
-   (string=
-    (format-time-string
-     "%FT%T%z"
-     (with-current-time repeat-todo--test-sunday
-                        (repeat-todo--next-scheduled-time
-                         repeat-todo--test-sunday repeat-todo--test-weekends)))
-    (format-time-string "%FT%T%z" repeat-todo--test-next-saturday)))
+   (time-string=
+    (with-current-time repeat-todo--test-tuesday
+                       (repeat-todo--next-scheduled-time
+                        repeat-todo--test-tuesday repeat-todo--test-weekdays))
+    repeat-todo--test-wednesday))
+  (should
+   (time-string=
+    (with-current-time repeat-todo--test-wednesday
+                       (repeat-todo--next-scheduled-time
+                        repeat-todo--test-wednesday repeat-todo--test-weekdays))
+    repeat-todo--test-thursday))
+  (should
+   (time-string=
+    (with-current-time repeat-todo--test-thursday
+                       (repeat-todo--next-scheduled-time
+                        repeat-todo--test-thursday repeat-todo--test-weekdays))
+    repeat-todo--test-friday))
+  (should
+   (time-string=
+    (with-current-time repeat-todo--test-friday
+                       (repeat-todo--next-scheduled-time
+                        repeat-todo--test-friday repeat-todo--test-weekdays))
+    repeat-todo--test-next-monday)))
 
+(ert-deftest repeat-todo--next-scheduled-time--weekends ()
   (should
-   (string=
-    (format-time-string
-     "%FT%T%z"
-     (with-current-time repeat-todo--test-monday
-                        (repeat-todo--next-scheduled-time
-                         repeat-todo--test-monday '(1 3 5))))
-    (format-time-string "%FT%T%z" repeat-todo--test-wednesday)))
+   (time-string=
+    (with-current-time repeat-todo--test-saturday
+                       (repeat-todo--next-scheduled-time
+                        repeat-todo--test-saturday repeat-todo--test-weekends))
+    repeat-todo--test-sunday))
   (should
-   (string=
-    (format-time-string
-     "%FT%T%z"
-     (with-current-time repeat-todo--test-wednesday
-                        (repeat-todo--next-scheduled-time
-                         repeat-todo--test-wednesday '(1 3 5))))
-    (format-time-string "%FT%T%z" repeat-todo--test-friday))))
+   (time-string=
+    (with-current-time repeat-todo--test-sunday
+                       (repeat-todo--next-scheduled-time
+                        repeat-todo--test-sunday repeat-todo--test-weekends))
+    repeat-todo--test-next-saturday)))
+
+(ert-deftest repeat-todo--next-scheduled-time--custom-numbers ()
+  (should
+   (time-string=
+    (with-current-time repeat-todo--test-monday
+                       (repeat-todo--next-scheduled-time
+                        repeat-todo--test-monday '(1 3 5)))
+    repeat-todo--test-wednesday))
+  (should
+   (time-string=
+    (with-current-time repeat-todo--test-wednesday
+                       (repeat-todo--next-scheduled-time
+                        repeat-todo--test-wednesday '(1 3 5)))
+    repeat-todo--test-friday))
+  (should
+   (time-string=
+    (with-current-time repeat-todo--test-monday
+                       (repeat-todo--next-scheduled-time
+                        repeat-todo--test-monday '(7)))
+    repeat-todo--test-sunday))
+  (should
+   (time-string=
+    (with-current-time repeat-todo--test-sunday
+                       (repeat-todo--next-scheduled-time
+                        repeat-todo--test-sunday '(7)))
+    repeat-todo--test-next-sunday)))
+
+(ert-deftest repeat-todo--parse-property--weekdays ()
+  (should
+   (equal (repeat-todo--parse-property "weekday") repeat-todo--test-weekdays))
+  (should
+   (equal (repeat-todo--parse-property "weekdays") repeat-todo--test-weekdays)))
+
+(ert-deftest repeat-todo--parse-property--weekends ()
+  (should
+   (equal (repeat-todo--parse-property "weekend") repeat-todo--test-weekends))
+  (should
+   (equal (repeat-todo--parse-property "weekends") repeat-todo--test-weekends)))
+
+(ert-deftest repeat-todo--parse-property--numbers ()
+  (should (equal (repeat-todo--parse-property "1 3 5") '(1 3 5)))
+  (should (equal (repeat-todo--parse-property "7") '(7)))
+  (should (equal (repeat-todo--parse-property "0") '(7))))
+
+(ert-deftest repeat-todo--parse-property--name-monday ()
+  (should (equal (repeat-todo--parse-property "m") '(1)))
+  (should (equal (repeat-todo--parse-property "mon") '(1)))
+  (should (equal (repeat-todo--parse-property "monday") '(1)))
+  (should (equal (repeat-todo--parse-property "M") '(1)))
+  (should (equal (repeat-todo--parse-property "Mon") '(1)))
+  (should (equal (repeat-todo--parse-property "Monday") '(1))))
+
+(ert-deftest repeat-todo--parse-property--name-tuesday ()
+  (should (equal (repeat-todo--parse-property "t") '(2)))
+  (should (equal (repeat-todo--parse-property "tue") '(2)))
+  (should (equal (repeat-todo--parse-property "tues") '(2)))
+  (should (equal (repeat-todo--parse-property "tuesday") '(2)))
+  (should (equal (repeat-todo--parse-property "T") '(2)))
+  (should (equal (repeat-todo--parse-property "Tue") '(2)))
+  (should (equal (repeat-todo--parse-property "Tues") '(2)))
+  (should (equal (repeat-todo--parse-property "Tuesday") '(2))))
+
+(ert-deftest repeat-todo--parse-property--name-wednesday ()
+  (should (equal (repeat-todo--parse-property "w") '(3)))
+  (should (equal (repeat-todo--parse-property "wed") '(3)))
+  (should (equal (repeat-todo--parse-property "wednesday") '(3)))
+  (should (equal (repeat-todo--parse-property "W") '(3)))
+  (should (equal (repeat-todo--parse-property "Wed") '(3)))
+  (should (equal (repeat-todo--parse-property "Wednesday") '(3))))
+
+(ert-deftest repeat-todo--parse-property--name-thursday ()
+  (should (equal (repeat-todo--parse-property "r") '(4)))
+  (should (equal (repeat-todo--parse-property "R") '(4)))
+
+  (should (equal (repeat-todo--parse-property "thu") '(4)))
+  (should (equal (repeat-todo--parse-property "thur") '(4)))
+  (should (equal (repeat-todo--parse-property "thurs") '(4)))
+  (should (equal (repeat-todo--parse-property "thursday") '(4)))
+  (should (equal (repeat-todo--parse-property "Thu") '(4)))
+  (should (equal (repeat-todo--parse-property "Thur") '(4)))
+  (should (equal (repeat-todo--parse-property "Thurs") '(4)))
+  (should (equal (repeat-todo--parse-property "Thursday") '(4))))
+
+(ert-deftest repeat-todo--parse-property--name-friday ()
+  (should (equal (repeat-todo--parse-property "f") '(5)))
+  (should (equal (repeat-todo--parse-property "fri") '(5)))
+  (should (equal (repeat-todo--parse-property "friday") '(5)))
+  (should (equal (repeat-todo--parse-property "F") '(5)))
+  (should (equal (repeat-todo--parse-property "Fri") '(5)))
+  (should (equal (repeat-todo--parse-property "Friday") '(5))))
+
+(ert-deftest repeat-todo--parse-property--name-saturday ()
+  (should (equal (repeat-todo--parse-property "sa") '(6)))
+  (should (equal (repeat-todo--parse-property "sat") '(6)))
+  (should (equal (repeat-todo--parse-property "saturday") '(6)))
+  (should (equal (repeat-todo--parse-property "Sa") '(6)))
+  (should (equal (repeat-todo--parse-property "Sat") '(6)))
+  (should (equal (repeat-todo--parse-property "Saturday") '(6))))
+
+(ert-deftest repeat-todo--parse-property--name-sunday ()
+  (should (equal (repeat-todo--parse-property "su") '(7)))
+  (should (equal (repeat-todo--parse-property "sun") '(7)))
+  (should (equal (repeat-todo--parse-property "sunday") '(7)))
+  (should (equal (repeat-todo--parse-property "Su") '(7)))
+  (should (equal (repeat-todo--parse-property "Sun") '(7)))
+  (should (equal (repeat-todo--parse-property "Sunday") '(7))))
+
+(ert-deftest repeat-todo--parse-property--name ()
+  (should
+   (equal (repeat-todo--parse-property "m t w r f sa su") '(1 2 3 4 5 6 7)))
+  ;; Mixed cases are supported
+  (should (equal (repeat-todo--parse-property "M f Sa") '(1 5 6)))
+  ;; They don't have to be in order
+  (should
+   (equal (repeat-todo--parse-property "Sunday Tuesday Monday") '(7 2 1))))
--- repeat-todo.el
+++ repeat-todo.el
@@ -55,7 +55,7 @@ One of:
 
   For example: (1) \"M\" or \"Mon\" \"Monday\"; (2) \"M T W\"
 
-- One or more of the days of the week as numbers; sunday=0, monday=1, etc:
+- One or more of the days of the week as numbers; sunday=0 or 7, monday=1, etc:
 
   For example: \"1 3 5\" for Mon, Wed, Fri "
   :group 'repeat-todo
@@ -63,38 +63,48 @@ One of:
 
 (defun repeat-todo--parse-property (prop-value)
   "Return repeating days for PROP-VALUE as list of day numbers (sun=0, mon=1, etc)."
-  (let ((case-fold-search nil))
+  (let ((case-fold-search t))
     (cond
      ((string-match "weekday\\(s\\)?" prop-value)
       '(1 2 3 4 5))
      ((string-match "weekend\\(s\\)?" prop-value)
-      '(6 0))
+      '(6 7))
+     ((string-match "[0-9]+" (replace-regexp-in-string " " "" prop-value))
+      ;; We use 7's internally to represent Sunday. However, users can specify
+      ;; Sunday with either 0 or 7.
+      (cl-subst 7 0
+                (mapcar
+                 'string-to-number (string-split prop-value " " 'omit-nulls))
+                :test 'eq))
      (t
       (let ((repeat-days '())
-            (prop-values (string-split " " prop-value 'omit-nulls)))
+            (prop-values (string-split prop-value " " 'omit-nulls)))
         (dolist (value prop-values)
-          ;; su, sun, sunday
-          ((when (string-match "^su\\(n\\(day\\)?\\)?")
-             (push 0 repeat-days)))
-          ;; m, mon, monday
-          ((string-match "^m\\(on\\(day\\)?\\)?") (push 1 repeat-days))
-          ;; t, tue, tues, tuesday
-          ((when (string-match "^t\\(ue\\(s\\(day\\)?\\)?")
-             (push 2 repeat-days)))
-          ;; w, wed, wednesday
-          ((when (string-match "^w\\(ed\\(nesday\\)?\\)?")
-             (push 3 repeat-days)))
-          ;; r, thu, thur, thurs, thursday
-          ((when (or (string= "r")
-                     (string= "R")
-                     (string-match "thu\\(r\\(s\\(day\\)?\\)?\\)?"))
-             (push 4 repeat-days)))
-          ;; f, fri, friday
-          ((when (string-match "^f\\(ri\\(day\\)?\\)?")
-             (push 5 repeat-days)))
-          ;; sa, sat, saturday
-          ((when (string-match "^sa\\(t\\(urday\\)?\\)?")
-             (push 6 repeat-days))))
+          (cond
+           ;; su, sun, sunday
+           ((string-match "^su\\(n\\(day\\)?\\)?" value)
+            (push 7 repeat-days))
+           ;; m, mon, monday
+           ((string-match "^m\\(on\\(day\\)?\\)?" value)
+            (push 1 repeat-days))
+           ;; Test for Thursday before Tuesday becasue we can mis-match on the "^T"
+           ;; r, thu, thur, thurs, thursday
+           ((or (string= "r" value)
+                (string= "R" value)
+                (string-match "^thu\\(r\\(s\\(day\\)?\\)?\\)?" value))
+            (push 4 repeat-days))
+           ;; t, tue, tues, tuesday
+           ((string-match "^t\\(ue\\(s\\(day\\)?\\)?\\)?" value)
+            (push 2 repeat-days))
+           ;; w, wed, wednesday
+           ((string-match "^w\\(ed\\(nesday\\)?\\)?" value)
+            (push 3 repeat-days))
+           ;; f, fri, friday
+           ((string-match "^f\\(ri\\(day\\)?\\)?" value)
+            (push 5 repeat-days))
+           ;; sa, sat, saturday
+           ((string-match "^sa\\(t\\(urday\\)?\\)?" value)
+            (push 6 repeat-days))))
         (reverse repeat-days))))))
 
 (defun repeat-todo--p (pom)
